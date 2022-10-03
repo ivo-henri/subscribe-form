66920816a531f404d6784dc5111f84a0ea56aefd
@kaspar <team@crewnew.com>
2022-10-03 15:25:54 +0100

    Tekstid

---
+++ src/assets/tekstid.js
@@ -0,0 +1,53 @@
+const topText = [
+    {
+      id: 1,
+      txt: "Kas sa tahad, et su raha kasvaks stabiilselt, ilma selle jaoks ise suurt tegemata? 💡",
+    },
+    {
+      id: 100,
+      txt: "Kas sa otsid konstantset rahavoogu, mis voimaldaks Sulle rahu ja meeldivat keskkonda elamiseks? 🧘🏽‍♂️",
+    },
+    {
+      id: 200,
+      txt: "Kas Sa oled kunagi mõelnud, miks Eestis investeerimine nii tabu ja tundlik teema on? Miks ta tekitab sellist emotsiooni? 🤷🏽‍♂️",
+    },
+    {
+      id: 300,
+      txt: "Väga lihtsalt põhjusel, sest meie 🇪🇪 riik on alles 31 aastat tagasi taas-iseseisvunud. Inimesed kardavad seda, mida nad ei tea.",
+    },
+    {
+      id: 400,
+      txt: "Hirmu saab eemaldada, tuginedes kogukonnale, kel on antud valdkonnas rohkem kogemust. 🔑",
+    },
+    {
+      id: 500,
+      txt: "Usas on vanaisad, kes räägivad jõululaua taga, kuidas nende kasutatavad fondid või aktsiad on terve elu üles sõitnud. Palju meil selliseid vanaisasid on? ❌",
+    },
+  ];
+
+  const bottomText = [
+    {
+      id: 400,
+      txt: "Tänaseks olen ma juba pea 5 aastat igapäevaselt finantsturgudel osalenud. 👨🏽‍💻",
+    },
+    {
+      id: 500,
+      txt: "Selle ajaga on mul tekkinud 2 kapitali kasvatamise tööriista, mis mulle enim meeldivad. 🧰",
+    },
+    {
+      id: 600,
+      txt: "Siin on 5 minutiline video ülevaade kahest täielikult automatiseeritud tööriistast ja selgitus- kuidas ma neid kasutan.",
+    },
+    {
+      id: 700,
+      txt: "✅ 1-3h üles seadmine.",
+    },
+    {
+      id: 800,
+      txt: "✅ Juriidiliselt ja riiklikult (1 nendest) reguleeritud ettevõtted.",
+    },
+    {
+      id: 900,
+      txt: "Millal sa ärkad ja mõistad, et internetis ringleb terve kuhi lahendusi? 🔐",
+    },
+  ];
